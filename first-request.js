3eeb5d2e36e7e6793aca7098fe807ea35394e248
“Sam <sam.alsmadi@gmail.com>
2017-09-26 23:29:56 -0400

    Initial commit

---
+++ first-request.js
@@ -0,0 +1,12 @@
+var request = require("request");
+
+request('http://www.google.com', function(error, response, body){
+	if(error){
+		console.log("Something went wrong!");
+		console.log(error);
+	} else {
+		if (response.statusCode === 200){
+			console.log(body);
+		}
+	}
+});
